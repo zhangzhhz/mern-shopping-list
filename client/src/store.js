2f1bc4d6d73c7966791c6c5d436a4bba658185ba
Zhang ZH <zhangzhhz@gmail.com>
2020-09-26 08:21:45 +0000

    cleanup, removed uuid and react-thunk

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,8 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
-const initialState = {};
-
+// const initialState = {};
+// 
 // const store = createStore(rootReducer, initialState, compose(
 //   applyMiddleware(...middleware),
 //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
