785d5fff5aca6e85b31c0f92056add35b06c7817
Zhang ZH <zhangzhhz@gmail.com>
2020-09-27 06:17:17 +0000

    minor update

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // mongoURI: 'mongodb://localhost/mern-shopping',
-  mongoURI: 'mongodb+srv://<user>:<pass>@cluster0.9o1yx.azure.mongodb.net/mern-shopping?retryWrites=true&w=majority',
-}
